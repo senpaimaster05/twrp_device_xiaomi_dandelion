7e16af149ea65ec2091920c2e9d857d95d9d4a23
master <85785239+senpaimaster05@users.noreply.github.com>
2021-09-25 16:24:46 +0800

    dandelion: Specify a path to lun file on device

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,6 +138,7 @@ TW_INCLUDE_NTFS_3G := true
 TW_USE_TOOLBOX := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
 TW_MAX_BRIGHTNESS := 2047
 TW_DEFAULT_BRIGHTNESS := 900
 TW_Y_OFFSET := 80
