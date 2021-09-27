4736497f632ef2b254e55112e2c70df277be92b0
master <85785239+senpaimaster05@users.noreply.github.com>
2021-09-27 10:37:31 +0800

    dandelion: product use 32 bit

--- twrp_dandelion.mk
+++ twrp_dandelion.mk
@@ -15,6 +15,7 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common TWRP stuff.
